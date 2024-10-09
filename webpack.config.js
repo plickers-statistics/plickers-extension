a98620d65761cea6f420fd0fc1972e9f4e4ad3a4
nvd05 <nvd05@bk.ru>
2024-10-09 19:01:32 +0500

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+
+const GenerateJsonFromJsPlugin = require('generate-json-from-js-webpack-plugin');
+const webpack                  = require('webpack');
+const path                     = require('path');
+
+
+const folders = {
+	declarations : path.resolve(__dirname, 'declarations'),
+	additions    : path.resolve(__dirname, 'additions'),
+
+	build  : path.resolve(__dirname, 'build'),
+	source : path.resolve(__dirname, 'src'),
+};
+
+const files = {
+	manifest : path.resolve(folders.source, 'manifest.cjs'),
+
+	additions : path.resolve(folders.additions, 'includes.ts'),
+
+	background : path.resolve(folders.source, 'service-background', 'main.ts'),
+	client     : path.resolve(folders.source, 'service-client',     'main.ts'),
+};
+
+/** @type { webpack.Configuration } */
+module.exports = {
+	plugins: [
+		new GenerateJsonFromJsPlugin({
+			filename : 'manifest.json',
+			path     : files.manifest
+		}),
+	],
+
+	resolve: {
+		extensions: [ '.js', '.ts' ],
+
+		alias: {
+			declarations : folders.declarations,
+			src          : folders.source,
+		},
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader'
+			}
+		]
+	},
+
+	optimization: {
+		minimize: false
+	},
+
+	output: {
+		filename : '[name].js',
+		path     : folders.build
+	},
+
+	entry: {
+		additions : files.additions,
+
+		background : files.background,
+		client     : files.client,
+	},
+
+	devtool : 'inline-source-map',
+	mode    : 'development'
+};
