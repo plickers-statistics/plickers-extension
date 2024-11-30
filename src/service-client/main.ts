6c7b9c79995e98da388cf3add8688f4de2e0b812
nvd05 <nvd05@bk.ru>
2024-11-30 14:55:37 +0500

    Update main.ts

--- src/service-client/main.ts
+++ src/service-client/main.ts
@@ -2,8 +2,8 @@
 import { Lobby } from './lobby/Lobby';
 
 
-const tag_root    = document.querySelectorWithCheck('div#root', HTMLDivElement);
-const lobby = new Lobby(tag_root);
+const tag_root = document.querySelectorWithCheck('div#root', HTMLDivElement);
+const lobby    = new Lobby(tag_root);
 
 lobby.initialize();
 
