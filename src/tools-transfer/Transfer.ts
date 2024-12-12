de92ac87bdd1669fd89c5b208523e3f9fbfba603
nvd05 <nvd05@bk.ru>
2024-12-12 22:31:54 +0500

    added Transfer.queue

--- src/tools-transfer/Transfer.ts
+++ src/tools-transfer/Transfer.ts
@@ -5,12 +5,30 @@ import { TransferPing } from './TransferPing';
 
 export class Transfer extends TransferPing
 {
+	private readonly queue: any[] = [];
+
+	private sendQueue (): void
+	{
+		this.queue.forEach(option => this.connection.postMessage(option));
+		this.queue.length = 0;
+	}
+
 	public send <TKey extends keyof TransferClientEvents>(type: TKey, data: TransferClientEvents[TKey]): void
 	{
 		const option = { type, data };
 
-		// [FIXED] Uncaught Error: Extension context invalidated.
 		console.debug('[PORT | Client => Background] message', option);
-		this.is_connected.state && this.connection.postMessage(option);
+
+		// [FIXED] Uncaught Error: Extension context invalidated.
+		this.is_connected.state
+			? this.connection.postMessage(option)
+			: this.queue.push(option);
+	}
+
+	public constructor (...options: ConstructorParameters<typeof TransferPing>)
+	{
+		super(...options);
+
+		this.is_connected.events.once('refresh', this.sendQueue.bind(this));
 	}
 }
