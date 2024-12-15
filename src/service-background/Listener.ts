f3cdb2921ea53483fde50885fbf24179376eeed7
nvd05 <nvd05@bk.ru>
2024-12-15 17:07:03 +0500

    Update Listener.ts

--- src/service-background/Listener.ts
+++ src/service-background/Listener.ts
@@ -3,6 +3,7 @@ import { Runtime } from 'webextension-polyfill';
 
 import { ServerEnvironment } from 'src/tools-environment/ServerEnvironment';
 import { WebSocketTasks } from 'src/tools-websocket-tasks/WebSocketTasks';
+import { isPackageDTO } from 'src/tools-DTOs/PackageDTO';
 
 
 export class Listener
@@ -13,11 +14,41 @@ export class Listener
 	private declare closed_resolve : () => void;
 	public readonly closed_promise = new Promise<void>(resolve => this.closed_resolve = resolve);
 
+	private readonly resend_port_to_websocket = (message: unknown) => {
+		const json = JSON.stringify(message);
+
+		if (isPackageDTO(message) === false)
+		{
+			throw new TypeError(json);
+		}
+
+		if (message.type === 'ping')
+		{
+			return;
+		}
+
+		this.websocket.send(json);
+	};
+
+	private readonly resend_websocket_to_port = (event: MessageEvent) => {
+		const message = event.data;
+		const data    = JSON.parse(message);
+
+		if (isPackageDTO(data) === false)
+		{
+			throw new TypeError(message);
+		}
+
+		this.connection.postMessage(data);
+	};
+
 	private readonly close = () => {
 		this.connection.onDisconnect.removeListener(this.close);
+		this.connection.onMessage.removeListener(this.resend_port_to_websocket);
 
 		this.websocket.removeEventListener('close', this.close);
 		this.websocket.removeEventListener('open', this.open);
+		this.websocket.removeEventListener('message', this.resend_websocket_to_port);
 
 		this.connection.disconnect();
 		this.websocket.close();
@@ -40,30 +71,10 @@ export class Listener
 	)
 	{
 		this.connection.onDisconnect.addListener(this.close);
-		this.websocket.addEventListener('close', this.close);
+		this.connection.onMessage.addListener(this.resend_port_to_websocket);
 
-		// ===== ===== ===== ===== =====
-
-		// port => websocket
-		this.connection.onMessage.addListener(message => {
-			if ((message as any).type === 'ping')
-			{
-				return;
-			}
-
-			this.websocket.send(JSON.stringify(message));
-		});
-
-		// websocket => port
-		this.websocket.addEventListener('message', ev => {
-			const message = ev.data;
-			const data    = JSON.parse(message);
-
-			this.connection.postMessage(data);
-		});
-
-		// ===== ===== ===== ===== =====
-
+		this.websocket.addEventListener('close', this.close, { once: true });
 		this.websocket.addEventListener('open', this.open, { once: true });
+		this.websocket.addEventListener('message', this.resend_websocket_to_port);
 	}
 }
